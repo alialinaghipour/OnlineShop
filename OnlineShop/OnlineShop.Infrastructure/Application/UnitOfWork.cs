66a3a37dad9127bd3ede5345807493b0557f5bdb
alimmst <79074714+alimmst@users.noreply.github.com>
2021-03-17 15:19:45 +0330

    added-unit-of-work

---
+++ OnlineShop/OnlineShop.Infrastructure/Application/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Infrastructure.Application
+{
+    public interface UnitOfWork:IDisposable
+    {
+        void Complate();
+        Task ComplateAysnc();
+    }
+}
---
+++ OnlineShop/OnlineShop.Persistence.EF/EFUnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using OnlineShop.Infrastructure.Application;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Persistence.EF
+{
+    public class EFUnitOfWork : UnitOfWork
+    {
+        private readonly EFDataContext _context;
+
+        public EFUnitOfWork(EFDataContext context)
+        {
+            _context = context;
+        }
+
+        public void Complate()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task ComplateAysnc()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
