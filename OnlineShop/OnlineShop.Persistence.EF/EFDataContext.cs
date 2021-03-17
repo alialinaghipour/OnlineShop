e537400c4a4af7c03ee82a347e4f9d3d57292c04
alimmst <79074714+alimmst@users.noreply.github.com>
2021-03-17 14:20:49 +0330

    added-context

---
+++ OnlineShop/OnlineShop.Persistence.EF/EFDataContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace OnlineShop.Persistence.EF
+{
+    public class EFDataContext:DbContext
+    {
+        public EFDataContext(DbContextOptions<EFDataContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyAssemblyConfigurations(typeof(EFDataContext).Assembly);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<AccountingDocument> AccountingDocuments { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductEntry> ProductEntries { get; set; }
+        public DbSet<SalesInvoice> SalesInvoices { get; set; }
+        public DbSet<SalesItem> SalesItems { get; set; }
+        public DbSet<WarehouseItem> WarehouseItems { get; set; }
+    }
+}
